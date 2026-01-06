0ee516b99a4fba1ef944f6080734afced7fb8986
SwaroopDangal <swaroopdangal732@gmail.com>
2026-01-06 10:40:59 +0545

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+import { Route, Routes } from "react-router";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
