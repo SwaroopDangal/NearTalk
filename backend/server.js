7ac00c1a8bc2b279693364e4c6ab03442ff567a2
SwaroopDangal <swaroopdangal732@gmail.com>
2026-01-09 16:57:31 +0545

    regex error fixed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/group", groupRoutes);
 // production
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
